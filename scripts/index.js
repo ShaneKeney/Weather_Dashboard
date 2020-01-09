35d94b43826623279f40c95989345d8ab930487a
Shane Keney <shane@spikeball.com>
2020-01-09 18:36:26 -0500

    - started website functionality - created element templates

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,17 @@
+var cityInfoContainerTemplate = $(".cityInfo").clone();
+var fiveDayContainerTemplate = $("#fiveDayContainer").clone();
+var cityListElementTemplate = $(".cityList").clone();
+
 $(document).ready(function () {
+
+    //bind functionality to sidebar
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
     });
+
+    //remove on load placeholder templates
+    $(".cityInfo").remove();
+    $("#fiveDayContainer").remove();
+    $(".cityList").remove();
 });
