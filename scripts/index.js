c90d4ff758631c491990d2d2eaa71d3d6670d7c4
Shane Keney <shane@spikeball.com>
2020-01-10 00:41:05 -0500

    - added example text for search input - added taking in user input and storing

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@ var cityInfoContainerTemplate = $(".cityInfo").clone();
 var fiveDayContainerTemplate = $("#fiveDayContainer").clone();
 var cityListElementTemplate = $(".cityList").clone();
 
+var baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&apiKey=85e68b224ff5d7b63d454f3ff7f9e09e';
+
 $(document).ready(function () {
 
     //bind functionality to sidebar
@@ -10,8 +13,28 @@ $(document).ready(function () {
         $(this).toggleClass('active');
     });
 
+    //bind event listener to search city option
+    $('#searchCityBttn').on('click', searchCity);
+
     //remove on load placeholder templates
     $(".cityInfo").remove();
     $("#fiveDayContainer").remove();
     $(".cityList").remove();
 });
+
+function searchCity(event) {
+    event.preventDefault();
+    
+    let userInput = $("#inputCity").val().trim();
+    if(userInput === '') return;
+
+    let citySearch, countryCode;
+    if(userInput.includes(',')) {
+        citySearch = userInput.substr(0, userInput.indexOf(',')).trim();
+        countryCode = userInput.substr(userInput.indexOf(',') + 1, userInput.length - 1).trim();
+    } else {
+        citySearch = userInput;
+    }
+
+    
+}
